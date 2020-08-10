bb6e6abde61c6dd33b0da4a6fd6c519a5ea97b38
Rory Jarrard <rory@crucibledevlabs.com>
2020-08-10 09:01:59 -0600

    create form partial for messages, backend post method in messages

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Message do
+  member_action :create, :method => :post do
+  end
 
+  controller do
+    def create
+      redirect_to admin_users_path
+    end
+
+    def message_params
+      params.require(:message).permit(:message, :user_id)
+    end
+  end
 end
