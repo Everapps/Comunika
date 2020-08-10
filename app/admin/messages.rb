72a69ff458b8dbf7f9427ff08e77791d31dc4460
Rory Jarrard <rory@crucibledevlabs.com>
2020-08-10 09:19:58 -0600

    handle sending sms with twilio and standard errors, notifications on page

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -4,11 +4,39 @@ ActiveAdmin.register Message do
 
   controller do
     def create
-      redirect_to admin_users_path
+      @message = Message.new(message_params)
+      @user = User.find(@message.user_id)
+
+      respond_to do |format|
+        if @message.save
+          begin
+            init_twilio
+            sms = @client.messages.create(
+                from: ENV['TWILIO_NUMBER'],
+                to: @user.contact_number,
+                body: @message.message
+            )
+          rescue Twilio::REST::RestError => e
+            format.html { redirect_to "/admin/users/#{@user.id}/create_sms", notice: "Error: unable to send message to " + @user.name + ", invalid contact number" }
+          rescue StandardError => e
+            format.html { redirect_to "/admin/users/#{@user.id}/create_sms", notice: "Error: unable to send message to " + @user.name }
+          else
+            format.html { redirect_to admin_users_path, notice: "Message was successfully sent to #{@user.name}." }
+          end
+        else
+          format.html { redirect_to "/admin/users/#{@user.id}/create_sms", notice: "Error: unable to send message to " + @user.name }
+        end
+      end
     end
 
     def message_params
       params.require(:message).permit(:message, :user_id)
+    end
+
+    def init_twilio
+      account_sid = ENV['TWILIO_SID']
+      auth_token = ENV['TWILIO_TOKEN']
+      @client = Twilio::REST::Client.new account_sid, auth_token
     end
   end
 end
