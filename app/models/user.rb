0aab19ae2771c489fc832d91b9f71e8d78ebc7ca
jwp <park.joonw@gmail.com>
2021-11-05 00:14:46 -0500

    add contact number to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   belongs_to :community
   has_many  :messages
 
+  validates :contact_number, length: { is: 10 }, allow_blank: true
+
   ROLES = %w{ tenant owner manager }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_005637) do
+ActiveRecord::Schema.define(version: 2021_11_05_050843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_05_005637) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contact_number"
     t.index ["community_id"], name: "index_users_on_community_id"
   end
 
